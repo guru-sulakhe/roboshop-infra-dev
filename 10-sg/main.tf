3b5774af8f31bf95c54069a2c26318d440f4ce70
guru-sulakhe <sulakheguruprasad@gmail.com>
2025-03-15 14:36:22 +0530

    creating provider.tf,main.tf,variables.tf,data.tf,locals.tf

---
+++ 10-sg/main.tf
@@ -0,0 +1,9 @@
+module "mysql_sg" {
+    source "git::https://github.com/guru-sulakhe/terraform-aws-securitygroup.git"
+    project_name = var.project_name
+    environment = var.environment
+    sg_name = var.sg_name
+    vpc_id = var.vpc_id
+    common_tags = var.common_tags
+    sg_tags = var.sg_tags
+}
