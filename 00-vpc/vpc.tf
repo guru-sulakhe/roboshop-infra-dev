0d950870e01aa2859acf26204109d2a8223c0ccd
guru-sulakhe <sulakheguruprasad@gmail.com>
2025-03-15 13:27:53 +0530

    creating vpc.tf

---
+++ 00-vpc/vpc.tf
@@ -0,0 +1,10 @@
+module "vpc" {
+    source = "git::https://github.com/guru-sulakhe/terraform-aws-vpc-1.git"
+    vpc_cidr = 
+    project_name = 
+    environment = 
+    common_tags =
+    public_subnet_cidrs =
+    private_subnet_cidrs = 
+    
+}
