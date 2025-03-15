f86a7d6b22142f5d3dfda017c246abe674b800c2
guru-sulakhe <sulakheguruprasad@gmail.com>
2025-03-15 15:10:13 +0530

    creating modules node_sg,eks_control_plane_sg,ingress_alb_sg

--- 10-sg/main.tf
+++ 10-sg/main.tf
@@ -17,3 +17,33 @@ module "bastion_sg" {
     common_tags = var.common_tags
     #sg_tags = var.bastion_sg_tags
 }
+
+module "node_sg" {
+    source = "git::https://github.com/guru-sulakhe/terraform-aws-securitygroup.git"
+    project_name = var.project_name
+    environment = var.environment
+    sg_name = "node"
+    vpc_id = local.vpc_id
+    common_tags = var.common_tags
+    #sg_tags = var.bastion_sg_tags
+}
+
+module "eks_control_plane_sg" {
+    source = "git::https://github.com/guru-sulakhe/terraform-aws-securitygroup.git"
+    project_name = var.project_name
+    environment = var.environment
+    sg_name = "eks_control_plane"
+    vpc_id = local.vpc_id
+    common_tags = var.common_tags
+    #sg_tags = var.eks_control_plane_sg_tags
+}
+
+module "ingress_alb_sg" {
+    source = "git::https://github.com/guru-sulakhe/terraform-aws-securitygroup.git"
+    project_name = var.project_name
+    environment = var.environment
+    sg_name= "ingress_alb_sg"
+    vpc_id = local.vpc_id
+    common_tags = var.common_tags
+
+}
