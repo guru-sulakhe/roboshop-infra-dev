cb0edea935c94f3af2af11216c347c13e984380e
guru-sulakhe <sulakheguruprasad@gmail.com>
2025-03-16 14:00:57 +0530

    creating bastion.sh,provider.tf,variables.tf

---
+++ 20-bastion/variables.tf
@@ -0,0 +1,18 @@
+variable "project_name" {
+    default = "roboshop"
+}
+variable "environment" {
+    default = "dev"
+}
+variable "common_tags" {
+    default = {
+        Project = "roboshop"
+        Terraform = true
+        Environment = "dev"
+    }
+}
+variable "bastion_tags" {
+    default = {
+        Component = "bastion"
+    }
+}
