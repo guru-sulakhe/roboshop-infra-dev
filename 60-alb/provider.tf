fb621d85304e1e20fef71228e4ff539cd4fa331b
guru-sulakhe <sulakheguruprasad@gmail.com>
2025-03-22 17:02:25 +0530

    changing provider

--- 60-alb/provider.tf
+++ 60-alb/provider.tf
@@ -2,12 +2,12 @@ terraform {
     required_providers {
         aws = {
             source = "hashicorp/aws"
-            version = "5.64.0"
+            version = ">= 5.91.0"
         }
     }
     backend "s3" {
         bucket = "guru97s-remote-state"
-        key = "roboshop-infra-dev-acm"
+        key = "roboshop-infra-dev-alb"
         region = "us-east-1"
         dynamodb_table =  "guru97s-locking-dynamodb"
     }
