1f0ee1f1e21eba1e9b2ee15ed92aa1b2c73907dd
guru-sulakhe <sulakheguruprasad@gmail.com>
2025-03-23 17:04:41 +0530

    changing resource_name

--- 80-cdn/locals.tf
+++ 80-cdn/locals.tf
@@ -1,4 +1,4 @@
 locals {
-    resource = "${var.project_name}-${var.environment}"
+    resource_name = "${var.project_name}-${var.environment}"
     https_certificate_arn = data.aws_ssm_parameter.https_certificate_arn.value
 }
